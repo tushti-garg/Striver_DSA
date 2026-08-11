133f52b2db2bebadcbdc0b10da5d6b5126d4a70f
Tushti Garg <tushtigarg456@gmail.com>
2026-08-11 18:14:37 +0000

    Day 3 : Patterns

--- Learn_the_basics/Build up logic building/Easy & Medium/Q6.java
+++ Learn_the_basics/Build up logic building/Easy & Medium/Q6.java
@@ -1,17 +1,15 @@
 import java.util.*;
-
-public class Q5 {
+public class Q6 {
     public static void main(String[] args){
-        Scanner sc =new Scanner(System.in);
-        System.out.print("Enter n : ");
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the value of n : ");
         int n = sc.nextInt();
         for(int i = n;i>=1;i--){
             for(int j = 1;j<=i;j++){
-                System.out.print("*");
+                System.out.print(j);
             }
-            System.out.println();
+           System.out.println();
         }
-        
     }
     
 }
---
+++ Learn_the_basics/Build up logic building/Easy & Medium/Q7.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Q7 {
+    public static void main(String[] args){
+        Scanner sc= new Scanner(System.in);
+        System.out.print("Enter the value of n : ");
+        int n = sc.nextInt();
+        for(int i = 1;i<=n;i++){
+            
+        }
+    }
+    
+}
