9cba1cedc058a9ca107a61ac716cd555b210bcbc
Tushti Garg <tushtigarg456@gmail.com>
2026-08-12 18:53:44 +0000

    Day 4

---
+++ Learn_the_basics/Build up logic building/Easy & Medium/Q11.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class Q11 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner (System.in);
+        System.out.print("Enter the value of n : ");
+        int n = sc.nextInt();
+        for(int i = 1; i<= n;i++){
+            for(int j = 1 ;j<=i;j++){
+                int sum = i+j;
+                if(sum %2 == 0){
+                    System.out.print("1");
+                }else{
+                    System.out.print("0");
+                }
+            }
+                System.out.println();
+            }
+        }
+
+
+        
+    }
+    
+
---
+++ Learn_the_basics/Build up logic building/Easy & Medium/Q12.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class Q12 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of n : ");
+        int n = sc.nextInt();
+        for(int i = 1;i<=n;i++){
+            for(int j = 1;j<=i;j++){
+                System.out.print(j);
+            }
+            for(int j = 1;j<=2*(n-i);j++){
+                System.out.print(" ");
+            }
+            for(int j = i ; j>=1;j--){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+        
+    }
+    
+}
